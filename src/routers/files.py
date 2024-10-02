d9c4d065a4cce52d661a174479ebca2fd7a0d0aa
Mohan Raj <mraj16457@gmail.com>
2024-10-02 22:57:09 +0530

    FM-2: Implementation creations. 1. Implemented user, folder, file creation.

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from model import File
+from database import get_db
+from schema import FileCreate
+from utils import get_file_by_name
 
 
 router = APIRouter(
@@ -6,6 +13,17 @@ router = APIRouter(
     tags=["FileOperations"]
 )
 
-@router.post("/")
-def create_file():
-    return True
+@router.post("/", response_model=FileCreate)
+def create_file(file: FileCreate, db: Session = Depends(get_db)):
+    db_file = File(name=file.name, folder_id=file.folder_id)
+    db.add(db_file)
+    db.commit()
+    db.refresh(db_file)
+
+    file_details = get_file_by_name(file_name=db_file.name, db=db)
+    return JSONResponse(
+        content={
+            "message": "Flie has been created Sucessfully",
+            "folder": {"id": file_details.id, "name": file_details.name}
+        }
+    )
--- src/routers/folders.py
+++ src/routers/folders.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from database import get_db
+from model import Folder
+from schema import FolderCreate
+from utils import get_folder_by_name, get_user
 
 
 router = APIRouter(
@@ -7,5 +14,17 @@ router = APIRouter(
 )
 
 @router.post("/")
-def create_folder():
-    return True
+def create_folder(folder: FolderCreate, db: Session = Depends(get_db)):
+    db_user = get_user(db=db, user_id=folder.user_id)
+    db_folder = Folder(name=folder.name, parent_folder_id=folder.parent_folder_id, user_id=db_user.id)
+    db.add(db_folder)
+    db.commit()
+    db.refresh(db_folder)
+
+    folder_details = get_folder_by_name(folder_name=db_folder.name, db=db)
+    return JSONResponse(
+        content={
+            "message": "Folder has been created Sucessfully",
+            "folder": {"id": folder_details.id, "name": folder_details.name}
+        }
+    )
