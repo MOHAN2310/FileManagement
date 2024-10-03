4c0958701686040a387bd3376d8efe2c6d2367ee
Mohan Raj <mraj16457@gmail.com>
2024-10-04 02:49:16 +0530

    FM-7 Enhancement: Search resources. 1. Implementation of search resources by name.(folder and files)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from database import Base, engine
-from routers import ( users, files, folders)
+from routers import ( users, files, folders, search)
 
 
 app = FastAPI(title="File Management Operations",
@@ -12,8 +12,9 @@ app = FastAPI(title="File Management Operations",
     )
 
 app.include_router(users.router)
-app.include_router(files.router)
+app.include_router(search.router)
 app.include_router(folders.router)
+app.include_router(files.router)
 
 
 Base.metadata.create_all(bind=engine)
